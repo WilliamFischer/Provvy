083f900b2b6ecd36ab9de4afb50d2c8bd9fa6c05
William Fischer <william.fischer@ivvy.com>
2022-05-04 10:50:07 +1000

    Browser fixes

--- src/app/base/base.component.ts
+++ src/app/base/base.component.ts
@@ -1,27 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { MasterService } from '../master.service';
+import { WalletService } from '../wallet.service';
 
 @Component({
   selector: 'app-base',
   templateUrl: './base.component.html',
-  styleUrls: ['./base.component.scss']
+  styleUrls: ['./base.component.scss'],
 })
 export class BaseComponent implements OnInit {
+  display: string = 'landing';
 
-  display :  string = 'landing';
-
-  constructor(public master : MasterService) { }
+  constructor(
+    public master: MasterService,
+    public walletService: WalletService
+  ) {}
 
   ngOnInit(): void {
     this.getDisplay();
   }
 
-  getDisplay(){
-    this.master.displaying$.subscribe(d => this.display = d);
+  getDisplay() {
+    this.master.displaying$.subscribe((d) => (this.display = d));
   }
 
-  setDisplay(display){
+  setDisplay(display) {
     this.master.setDisplay(display);
   }
 
+  async connectToMetaMask() {
+    await this.walletService.connectAccount();
+  }
 }
--- src/app/wallet.service.ts
+++ src/app/wallet.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 
-import Web3 from "web3";
-import Web3Modal from "web3modal";
+//import Web3 from "web3";
+// import Web3Modal from "web3modal";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 
+declare const window: any;
 @Injectable({
   providedIn: 'root'
 })
@@ -14,7 +15,11 @@ export class WalletService {
   account: any;
   web3Modal: any;
 
+
+
   constructor() {
+    const Web3Modal = window.Web3Modal.default;
+
     const providerOptions = {
       walletconnect: {
         package: WalletConnectProvider, // required
@@ -47,26 +52,18 @@ export class WalletService {
     this.web3js.eth.net.getId().then(netId => {
 
       console.log(netId)
-      
+
       if(netId == 56){
         let model = this.web3js.eth.accounts.create();
 
         this.web3js.setProvider('https://bsc-dataseed1.binance.org:443', "unspecified"); // set to Binance
-    
+
         // Create Gacha Wallet
         this.web3js.eth.accounts.wallet.add({
           privateKey: model.privateKey,
           address: '0x4f4755D79c5c6f8987791cC5f6c4f3F414529Bd2'
         }, reason => {
           console.error('Get GACHA Error: ' + reason); // Error!
-        });
-    
-        // Create Gacha Premium Wallet
-        this.web3js.eth.accounts.wallet.add({
-            privateKey: model.privateKey,
-            address: '0x9509df81196c2376ad6c4c447dE29B09559A6f2B'
-        }, reason => {
-          console.error('Get GACHA+ Error: ' + reason); // Error!
         });
 
         this.saveUser();
@@ -77,6 +74,6 @@ export class WalletService {
   }
 
   saveUser(){
-    alert('DONE!')
+    alert('Connected to MetaMask!')
   }
 }
